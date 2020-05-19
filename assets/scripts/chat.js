353a729d30920946e2891d5114e6f651b0637198
Celso Hashimoto <celsoyh@gmail.com>
2020-05-19 12:56:59 -0300

    Change fn

--- assets/scripts/chat.js
+++ assets/scripts/chat.js
@@ -48,20 +48,27 @@ function rntOnFormSubmit() {
         email: "teste@gmail.com",
       });
 
-      window.fcWidget.user.setProperties({
-          "cpf": document.getElementById('rnt_companion_cpf').value,
-        
-      }).then(function() {
-        setTimeout(function () {
-          const companionFormComponent = document.getElementById('rnt_companionForm');
-          companionFormComponent.classList.remove('active');
-          companionFormComponent.classList.remove('overflow');
-        }, 500);
-  
-        setTimeout(function() {
-          window.fcWidget.open();
-        }, 1500);
-      })
+
+      window.fcWidget.on('user:created', function(resp) {
+        var status = resp && resp.status,
+        if (status === 200) {
+          window.fcWidget.user.setProperties({
+            "cpf": document.getElementById('rnt_companion_cpf').value,
+          
+        }).then(function() {
+          setTimeout(function () {
+            const companionFormComponent = document.getElementById('rnt_companionForm');
+            companionFormComponent.classList.remove('active');
+            companionFormComponent.classList.remove('overflow');
+          }, 500);
+    
+          setTimeout(function() {
+            window.fcWidget.open();
+          }, 1500);
+        })
+        }
+      });
+     
 
       
     }
